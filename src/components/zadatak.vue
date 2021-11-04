fea93e4bec20ac6a680613986def9c780eeee9fa
danieldodic <ddodic@tvz.hr>
2021-11-04 14:24:38 +0100

    Prvi Zadatak (dodavanje gumba)

---
+++ src/components/zadatak.vue
@@ -0,0 +1,16 @@
+<template>
+  <section>
+    <o-button @click="clickMe">Click Me</o-button>
+  </section>
+</template>
+
+<script>
+export default {
+    methods: {
+      clickMe() {
+        alert('Clicked!')
+      }
+    }
+  }
+
+</script>
